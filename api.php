a4dce3ea691f5388c64dc5a12ed07a90e78ddd5e
Mariogrip <mario.grip@gmail.com>
2013-06-16 11:21:49 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,19 +1,31 @@
 <?php
 
+
 include('ssl.inc.php');
 include('timezone.inc.php');
 
+create_graph("mhsav-hour.png", "-1h", "Hourly");
+create_graph("mhsav-day.png", "-1d", "Daily");
+create_graph("mhsav-week.png", "-1w", "Weekly");
+create_graph("mhsav-month.png", "-1m", "Monthly");
+create_graph("mhsav-year.png", "-1y", "Yearly");
 
+function create_graph($output, $start, $title) {
+  $RRDPATH = '/opt/minepeon/var/rrd/';
+  $options = array(
+    "--slope-mode",
+    "--start", $start,
+    "--title=$title",
+    "--vertical-label=Hash per second",
+    "--lower=0",
+    "DEF:hashrate=" . $RRDPATH . "hashrate.rrd:hashrate:AVERAGE",
+    "CDEF:realspeed=hashrate,1000,*",
+    "LINE2:realspeed#FF0000"
+  );
 
-include('head.php');
-include('menu.php');
-
+  $ret = rrd_graph("/opt/minepeon/http/rrd/" . $output, $options);
+  if (! $ret) {
+    echo "<b>Graph error: </b>".rrd_error()."\n";
+  }
+}
 ?>
-        <div class="container">
-                <h1>About</h1> 
-				<img align="right" title="MinePeonQR" alt="Donate to 12Ui8w9q6eq6TxZmow8H9VHbZdfEnsLDsB" src="https://mineforeman.com/wp-content/uploads/2013/01/MinePeonQR.png" width="200" height="200" />
-				<blockquote class="a">Peon's in feudal times were indentured vassals, often assigned dangerous, tedious and unpleasant work, like mining.<cite>MineForeman</cite></blockquote>
-        </div>
-<?php
-
-include('foot.php');
