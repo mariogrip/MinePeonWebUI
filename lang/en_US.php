a68f85950a1f902dba3f989442e34b9f3409b32a
Mariogrip <mario.grip@gmail.com>
2013-06-16 12:31:01 +0300

    Adding stuff to EN

--- lang/en_US.php
+++ lang/en_US.php
@@ -1,2 +1,15 @@
 //Lang file, you can use this as template if you wanna translate
-//
+//This is not near Finied, I will do some more stuff here!
+
+//Menu
+$l_graphs = "Graphs";
+$l_pools = "Pools";
+$l_settings = "Settings";
+$l_about = "About";
+$l_contact = "Contact";
+$l_license = "License";
+
+//Index
+$l_update_time = "Update Time";
+$l_graph_error = "Graph error";
+
