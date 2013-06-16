dadb36f8f82b363f7022a4fb26d8c0098e682064
Mariogrip <mario.grip@gmail.com>
2013-06-16 12:33:22 +0300

    Translate...

--- lang/no_NB.php
+++ lang/no_NB.php
@@ -1,3 +1,15 @@
 //Norwegian
 //Norsk Bokmål Flag: NO
 
+//Menu
+$l_graphs = "Statestik";
+$l_pools = "Pools";
+$l_settings = "Instilinger";
+$l_about = "Om oss";
+$l_contact = "Kontakt";
+$l_license = "Lisens";
+
+//Index
+$l_update_time = "Oppdaterings tid";
+$l_graph_error = "Statestik feil";
+
