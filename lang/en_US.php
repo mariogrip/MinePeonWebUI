71b07d61f83b844c8b0a9f4ea82fd869f71c73bb
Mariogrip <mario.grip@gmail.com>
2013-06-17 18:52:26 +0300

    Adding more stuff to EN

--- lang/en_US.php
+++ lang/en_US.php
@@ -13,3 +13,21 @@ $l_license = "License";
 $l_update_time = "Update Time";
 $l_graph_error = "Graph error";
 
+//Pool
+$l_url = "URL";
+$l_username = "Username";
+$l_password = "Password";
+$l_pooltext1 = "Use this form to change to your own mining accounts!  Pressing submit will take 60 seconds as the miner restarts with the new configuration.":
+$l_pooltext2 = "There is very little validation on these settings at the moment so make sure your settings are correct!";
+$l_pooltext3 = "While I don't mind if you leave my details you will be mining using the MinePeon test account and any bitcoins generated will be concidered a dontation to the MinePeon project."
+$l_warning = "WARNING";
+
+//settings
+$l_new_password = "New Password";
+$l_timezone = "TimeZone";
+
+
+//about
+$l_abouttext = "Peon's in feudal times were indentured vassals, often assigned dangerous, tedious and unpleasant work, like mining.";
+
+//contact
