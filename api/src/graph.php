cc677991ef0d1a40ee29f4cb1ee8988f26d80728
mariogrip <mario.grip@gmail.com>
2013-08-16 17:04:29 +0200

    Adding more

--- api/src/graph.php
+++ api/src/graph.php
@@ -10,27 +10,27 @@ if (isset($_GET["API_KEY"])){
 
 if ($_GET["type"] == "hour"){
 ?>
-<img src="rrd/mhsav-hour.png" alt="mhsav.png" />
+<img src="../../rrd/mhsav-hour.png" alt="mhsav.png" />
 <?php
 }
 if ($_GET["type"] == "day"){
 ?>
-<img src="rrd/mhsav-day.png" alt="mhsav.png" />
+<img src="../../rrd/mhsav-day.png" alt="mhsav.png" />
 <?php
 }
 if ($_GET["type"] == "week"){
 ?>
-<img src="rrd/mhsav-week.png" alt="mhsav.png" />
+<img src="../../rrd/mhsav-week.png" alt="mhsav.png" />
 <?php
 }
 if ($_GET["type"] == "month"){
 ?>
-<img src="rrd/mhsav-month.png" alt="mhsav.png" />
+<img src="../../rrd/mhsav-month.png" alt="mhsav.png" />
 <?php
 }
 if ($_GET["type"] == "year"){
 ?>
-<img src="rrd/mhsav-year.png" alt="mhsav.png" />
+<img src="../../rrd/mhsav-year.png" alt="mhsav.png" />
 <?php
 }
 
