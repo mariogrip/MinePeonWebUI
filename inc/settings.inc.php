fb465de495f34977154a810de7e0b7aa97888a0d
thgh <thoogh@gmail.com>
2013-08-28 23:11:50 +0200

    Should not have removed this

--- inc/settings.inc.php
+++ inc/settings.inc.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!isset($_SERVER['HTTPS']) and php_sapi_name() <> 'cli') {
+	header('Location: https://' . $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI']);
+}
+
 $settings = json_decode(@file_get_contents('/opt/minepeon/etc/minepeon.conf'), true);
 
 if(isset($settings['userTimezone'])){
